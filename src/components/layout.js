6930922da4ded7aece581db06188881048f65812
PhotolyzeAnt <none>
2020-10-15 11:26:02 +0200

    login wip

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { IdentityContextProvider } from "react-netlify-identity-widget"
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
+  <IdentityContextProvider url="https://nervous-dijkstra-0c7e88.netlify.app">
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,19 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
+import { useIdentityContext } from "react-netlify-identity"
 
 const Profile = () => {
+  const identity = useIdentityContext()
+  const isLoggedIn = identity && identity.isLoggedIn
+
+  console.log(identity)
+
   return (
-    <div className="dashboard-header">
-      <nav>
-        <Link to="/dashboard/secret" activeClassName="active">
-          Secret stuff
-        </Link>
-        <Link to="/dashboard/base" activeClassName="active">
-          See your base
-        </Link>
-      </nav>
-      <span>TODO show login status</span>
-    </div>
+    isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link to="/dashboard/secret" activeClassName="active">
+            Secret stuff
+          </Link>
+          <Link to="/dashboard/base" activeClassName="active">
+            See your base
+          </Link>
+        </nav>
+        <span>TODO show login status</span>
+      </div>
+    )
   )
 }
 
